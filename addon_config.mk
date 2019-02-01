d6bf11b535bc34def405146813360b76c2ec59fd
Memo Akten <memo@memo.tv>
2019-02-01 04:50:41 +0300

    addon_config

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxMSADataPathManager
+	ADDON_DESCRIPTION = Utils to manage data path
+	ADDON_AUTHOR = Memo Akten, www.memo.tv
+	ADDON_TAGS = ""
+	ADDON_URL = https://github.com/memo/ofxMSADataPathManager
+
+common:
+	# dependencies with other addons, a list of them separated by spaces
+	# or use += in several lines
+	ADDON_DEPENDENCIES =
